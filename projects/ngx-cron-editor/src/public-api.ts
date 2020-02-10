749037f9e442f1929234e916642efa5522672d2f
a-pradhan <ashish.pradhan@ats-global.com>
2020-02-10 10:25:41 +0000

    bug: fixes public api exported components

--- projects/ngx-cron-editor/src/public-api.ts
+++ projects/ngx-cron-editor/src/public-api.ts
@@ -2,16 +2,13 @@
  * Public API Surface of ngx-cron-editor
  */
 
+export * from './lib/Components/Day/day-expression.component';
+export * from './lib/Components/expression-selector/expression-selector.component';
+export * from './lib/Model/cron-expression';
+export * from './lib/Model/enums';
 export * from './lib/ngx-cron-editor.component';
 export * from './lib/ngx-cron-editor.module';
-export * from './lib/Model/enums';
-export * from './lib/Model/options';
-export * from './lib/ngx-cron-time-picker.component';
-export * from './lib/Components/Day/daily-scheduler.component';
-export * from './lib/components/Hourly/hourly-scheduler.component';
-export * from './lib/Model/minute-cron-expression-model';
-export * from './lib/components/Minute/minute-cron-expression.component';
-export * from './lib/Model/cron-expression';
+
 
 
 
